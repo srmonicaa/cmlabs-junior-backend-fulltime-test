a2f2c393243a04025f350e6a1310ba5555cc7ea9
monica <monica_sari@klik-ss.com>
2023-07-15 09:48:23 +0700

    first commit

---
+++ palindrome-test.js
@@ -0,0 +1,6 @@
+console.log(isPalindrome("SAIPPUAKIVIKAUPPIAS")); // true
+console.log(isPalindrome("Aibohphobia")); // true
+console.log(isPalindrome("Anna")); // true
+console.log(isPalindrome("Civic")); // true
+console.log(isPalindrome("My gym")); // true
+console.log(isPalindrome("No lemon, no melon")); // false
